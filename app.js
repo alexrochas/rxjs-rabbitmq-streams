5ab93d59a0933f7dbd122120210a2a32f7a511b4
Alex Rocha <alex.rochas@yahoo.com.br>
2017-06-22 20:00:49 -0300

    refactor: remove trash

--- app.js
+++ app.js
@@ -6,17 +6,10 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
 var amqp = require('./routes/amqp');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,7 +17,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/amqp', amqp);
 
 // catch 404 and forward to error handler
